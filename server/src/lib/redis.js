7ed429360b233ae936d6023ca5a9b1ab5653cee1
nirbhayjakhar3196 <jakharnirbhay0000@gmail.com>
2026-07-24 15:50:04 +0530

    feat:Added redis env

--- server/src/lib/redis.js
+++ server/src/lib/redis.js
@@ -1,7 +1,13 @@
-import IORedis from "ioredis"
+import IORedis from "ioredis";
 
-export const redis = new IORedis({
-    host: "127.0.0.1",
-    port: 6379,
-    maxRetriesPerRequest: null,
-})
+export const redis = new IORedis(process.env.REDIS_URL, {
+  maxRetriesPerRequest: null,
+});
+
+redis.on("connect", () => {
+  console.log("✅ Redis Connected");
+});
+
+redis.on("error", (err) => {
+  console.error("❌ Redis Error:", err.message);
+});
